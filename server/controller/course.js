bf591b55cb1ce631ea008b51f0cf903a11bc2487
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2020-12-29 20:34:05 -0300

    put y delete agregado a course

--- server/controller/course.js
+++ server/controller/course.js
@@ -3,21 +3,25 @@ const mysqlConnection = require('../database/database');
 const app = express();
 
 app.get('/course', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Course', (err, customers) => {
+    mysqlConnection.query('SELECT * FROM Course', (err, courses) => {
         if(err) return res.status(400).json({err});
-        res.json(customers);
+        if(Object.entries(courses).length == 0) return res.status(400).json({
+            ok: false,
+            message: "No existen platillos registrados"
+        });
+        res.json(courses);
     });
 })
 
 app.get('/course/:id', (req, res) => {
     const {id} = req.params;
-    mysqlConnection.query('SELECT * FROM Course WHERE idCourse = ?', [id], (err, customers) => {
+    mysqlConnection.query('SELECT * FROM Course WHERE idCourse = ?', [id], (err, courses) => {
         if(err) return res.status(400).json({err});
-        if(Object.entries(customers).length == 0) return res.status(400).json({
+        if(Object.entries(courses).length == 0) return res.status(400).json({
             ok: false,
-            message: "No encontrado"
+            message: "Platillo no encontrado"
         });
-        res.json(customers);
+        res.json(courses);
     });
 });
 
@@ -28,12 +32,42 @@ app.post('/course', (req, res) => {
         if(err) return res.status(400).json({err});
         res.json({
             ok: true,
-            course: {
-                Name,
-                Stock,
-                Price,
-                Description
-            }
+            course: {Name, Stock, Price, Description}
+        });
+    });
+});
+
+app.put('/course/:id', (req, res) => {
+    let id = req.params.id;
+    let body = req.body;
+    mysqlConnection.query('SELECT * FROM Course WHERE idCourse = ?', id, (err, findcourse) => {
+        if(err) return res.status(400).json({err});
+        if(Object.entries(findcourse).length == 0) return res.status(400).json({
+            ok: false,
+            message: "Platilo no encontrado"
+        });
+        delete body.idCourse;
+        mysqlConnection.query('UPDATE Course SET ? WHERE idCourse = ?', [body, id], (err, courses) => {
+            if(err) return res.status(400).json({err});
+            res.json({
+                ok: true,
+                body
+            });
+        });
+    });
+});
+
+app.delete('/course/:id', (req, res) => {
+    let id = req.params.id;
+    mysqlConnection.query('SELECT * FROM Course WHERE idCourse = ?', id, (err, findCourse) =>{
+        if(err) return res.status(400).json({err});
+        if(Object.entries(findCourse).length == 0) return res.status(400).json({
+            ok: false,
+            message: "Platillo no encontrado"
+        });
+        mysqlConnection.query('UPDATE Course SET ? WHERE idCourse = ?', [{Stock: 0}, id], (err, courses) => {
+            if(err) return res.status(400).json({err});
+            res.json({ok: true});
         });
     });
 });
