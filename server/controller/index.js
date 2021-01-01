aa94498eb4ba70bd5d769589cc6b2b715bafc3eb
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-01-01 16:44:37 -0300

    Añadido el login

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,5 +4,6 @@ const app = express();
 app.use(require("./customer"));
 app.use(require("./admin"));
 app.use(require("./course"));
+app.use(require("./login"));
 
 module.exports = app;
---
+++ server/controller/login.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mysqlConnection = require('../database/database');
+const app = express();
+
+app.post('/login', (req, res) => {
+    let body = req.body;
+    mysqlConnection.query('SELECT * FROM User WHERE Email = ?', body.Email, (err, userDB) => {
+        if(Object.entries(userDB).length == 0) return res.status(400).json({
+            ok: false,
+            message: "EMAIL o contraseña invalidos"
+        });
+        if(!bcrypt.compareSync(body.Password, userDB[0].Password)) return res.status(400).json({
+            ok: false,
+            message: "Email o CONTRASEÑA invalidos"
+        });
+        let {idUser, Name, LastName, Email, Img} = userDB[0];
+        let token = jwt.sign(
+            {user: {idUser, Name, LastName, Email, Img}},
+            process.env.SEED,
+            {expiresIn: process.env.EXPIRATION_TOKEN}  
+        );
+        res.json({
+            user: {idUser, Name, LastName, Email, Img},
+            token
+        });
+    });
+});
+
+module.exports = app;
